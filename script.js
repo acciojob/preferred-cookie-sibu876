956b69784459e1b541471cd8154af1f6e5e46696
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-08-03 17:15:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 //your JS code here. If required.
+
+const save = document.getElementById("save");
+const fontSize = document.getElementById("fontsize");
+const fontcolor = document.getElementById("fontcolor");
+
+function getCookies(name){
+	const cookies = document.cookie.split(";");
+	for(let cookie of cookies){
+	    const [key, value] = cookie.trim().split("=");
+		if(key === name){
+			return decodeURIComponent(value);
+		}
+	}
+
+
+	return null;
+}
+
+const savedfontsize = getCookies("fontsize");
+const savedcolor = getCookies("fontcolor");
+
+if(savedfontsize){
+	fontSize = savedfontsize;
+	document.doccumentElement.style.setProperty(
+		"--fontsize",
+		savedfontsize + "px";
+	)
+}
+
+if(savedcolor){
+	fontcolor = savedcolor;
+	document.documentElement.style.setProperty(
+		"--fontcolor",
+		savedcolor 
+		
+	)
+}
+
+save.addEventListener('click',(e) =>{
+	e.preventDefault();
+
+	document.cookie = `fontsize=${fontSize.value}`
+	document.cookie = `fontcolor=${fontcolor.value}`
+	
+	document.documentElement.style.setProperty(
+		"--fontcolor",
+		fontSize.value;
+		
+	)
+
+	document.documentElement.style.setProperty(
+		"--fontcolor",
+		fontcolor.value;
+		
+	)
+})
